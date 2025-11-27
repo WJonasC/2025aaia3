9d5fe51c2f43e86c9fc279d046415151640b7283
WJonasC <s011421@std.hwhs.tc.edu.tw>
2025-11-27 16:47:09 +0800

    week12

---
+++ week 12/week 12-03.cpp
@@ -0,0 +1,36 @@
+/// week 12-03.cpp 學習計畫 Math 倒數第 2 題
+/// LeetCode 43. Multiply Strings
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if (num1=="0" || num2 == "0") return "0"; /// 陷阱：0 乘上任何數都是 0
+        int N1 = num1.length(), N2 = num2.length();
+        vector <int> ans(N1+N2); /// 這麼多位
+        for (int i = N1-1; i >= 0; i--) /// 兩層暴力 for 迴圈
+        {
+            for (int j = N2-1; j >= 0; j--) /// 倒過來的迴圈
+            {
+                /// num1[i] vs. num2[j] 要記得把字母變數值
+                ans [i+j+1] += (num1[i]-'0') * (num2[j]-'0');
+                if (ans[i+j+1]>9) ans[i+j] += ans[i+j+1] / 10;
+                ans[i+j+1] = ans[i+j+1] %10; /// 餘數
+            }
+        } /// 還沒有完成！！！！
+        string strAns; /// 答案的字串
+        if (ans[0]==0) ///沒有進位、最前面是0ㄚㄚㄚㄚ
+        {
+            for (int i=1; i<N1+N2; i++) /// 只好避開最前面的0拉
+            {
+                strAns += (char)(ans[i]+'0');
+            }
+        }
+        else /// 有進位，湊齊 N1 + N2 位
+        {
+            for (int i=0; i<N1+N2; i++)
+            {
+                strAns += (char)(ans[i]+'0');
+            }
+        } /// 還沒完成！！
+        return strAns;
+    }
+};
